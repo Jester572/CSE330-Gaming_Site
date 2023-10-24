42994f0cc5fe2d370b895db0c44fa4272d6a623b
Jester572 <jester572@yahoo.com>
2023-10-24 15:18:27 -0600

    changed cover to coverImage

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -13,15 +13,15 @@ mainCont.buildFeaturedGames = async function (req, res, next) {
             features += "<div class='game_card'>";
             const game = await API.fetchGameById(game_id);
             console.log(game[0]);
-            const cover = await API.fetchCoverById(game[0].cover);
-            console.log(cover[0].image_id);
+            const coverImage = await API.fetchCoverById(game[0].cover);
+            console.log(coverImage[0].image_id);
             features += "<h3 class='game_name'>" + game[0].name + "</h3>";
             features += "<p class='game_summary'>" + game[0].summary + "</p>";
             for (const genre_id of game[0].genres) {
                 const genres = await API.fetchGenreById(genre_id);
                 features += "<p class='game_genre'>" + genres[0].slug + "</p>";
             }
-            features += "<img class='game_cover' src='https://images.igdb.com/igdb/image/upload/t_cover_small_2x/" + cover[0].image_id + ".jpg' alt='Cover image for" + game[0].name + "'></img>";
+            features += "<img class='game_cover' src='https://images.igdb.com/igdb/image/upload/t_cover_small_2x/" + coverImage[0].image_id + ".jpg' alt='Cover image for" + game[0].name + "'></img>";
             game_data.push(game[0]);
             features += "</div>";
 
